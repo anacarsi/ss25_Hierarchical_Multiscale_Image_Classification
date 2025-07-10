3a118f98785382e14bca62705f738d9447f28488
anacarsi <ana.carsi.gonzalez@gmail.com>
2025-07-10 07:05:36 +0200

    solved flag color

--- src/datasets/patch_dataset.py
+++ src/datasets/patch_dataset.py
@@ -8,9 +8,10 @@ from collections import Counter # For counting class distributions
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
+    DEBUG = '\033[96m'
+    INFO = '\033[95m'      # pink
+    WARNING = '\033[93m'   # yellow
+    ERROR = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
